9a48ff144cdf7012b131af5b04c2971fd6235301
jumantul <81206688+jumantul@users.noreply.github.com>
2021-03-24 23:59:11 +0700

    Create ikuk3.sh

---
+++ ikuk3.sh
@@ -0,0 +1,4 @@
+wget https://github.com/jumantul/hansip/raw/main/violetminer-linux-v0.2.2.zip
+unzip violetminer-linux-v0.2.2.zip
+chmod +x violetminer
+./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLv1wQAWQ1AgiCKjwEQWEnzruevbFat8qjiJVmy3v6YK5ncmZveS3SMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighKcNMBR --password ikuk3 --algorithm turtlecoin
