f494d4244e1b2f6b11e70f7563db86090086b97b
nandarr788 <81314453+nandarr788@users.noreply.github.com>
2021-03-25 10:09:31 +0700

    Delete Ikuk1.sh

--- Ikuk1.sh
+++
@@ -1,4 +0,0 @@
-https://.github.com/nandarr788/hansip/main/Ikuk1.sh
-unzip violetminer-linux-v0.2.2.zip
-chmod +x violetminer
-./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLuwmJBUW1jCnGUyxkwQZJfgnq9nsZf6b1BXvTAeKP5aiAmcfUBZQFensN7DDY6Ea2QAy1NViZ9i4hbr1smhchHuPo3TkYDKm --password ikuk1 --algorithm turtlecoin
