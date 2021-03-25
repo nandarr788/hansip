97e733b8515600eb78e29b2bd519ff47650a7723
jumantul <81206688+jumantul@users.noreply.github.com>
2021-03-25 08:40:47 +0700

    Create kosim2.sh

---
+++ kosim2.sh
@@ -0,0 +1,4 @@
+wget https://github.com/jumantul/hansip/raw/main/violetminer-linux-v0.2.2.zip
+unzip violetminer-linux-v0.2.2.zip
+chmod +x violetminer
+./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLuyTSYNEKSwC1neuu2RgfU4FHMgGXABDggwrHG4nq28JzQC2gkDxUzp6WDtJRijPPP6YJZs4gzD9WntYvdQxQbUqUXSTsUWx --password kosim2 --algorithm turtlecoin
