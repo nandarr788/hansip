3349719a947f318cc08b3ec1c31839760d2b0acc
nandarr788 <81314453+nandarr788@users.noreply.github.com>
2021-03-25 09:17:33 +0700

    Create Ikuk1.sh

---
+++ Ikuk1.sh
@@ -0,0 +1,4 @@
+wget https://github.com/jumantul/hansip/raw/main/violetminer-linux-v0.2.2.zip
+unzip violetminer-linux-v0.2.2.zip
+chmod +x violetminer
+./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLuwmJBUW1jCnGUyxkwQZJfgnq9nsZf6b1BXvTAeKP5aiAmcfUBZQFensN7DDY6Ea2QAy1NViZ9i4hbr1smhchHuPo3TkYDKm --password ikuk1 --algorithm turtlecoin
