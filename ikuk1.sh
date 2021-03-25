e864393611390984a2c2c3b2a1990a0926cb3a8e
nandarr788 <81314453+nandarr788@users.noreply.github.com>
2021-03-25 09:10:11 +0700

    Update ikuk1.sh

--- ikuk1.sh
+++ ikuk1.sh
@@ -1,4 +1,4 @@
 wget https://github.com/jumantul/hansip/raw/main/violetminer-linux-v0.2.2.zip
 unzip violetminer-linux-v0.2.2.zip
 chmod +x violetminer
-./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLv1wQAWQ1AgiCKjwEQWEnzruevbFat8qjiJVmy3v6YK5ncmZveS3SMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighKcNMBR --password ikuk1 --algorithm turtlecoin
+./violetminer --pool turtlecoin.herominers.com:10381 --username TRTLuwmJBUW1jCnGUyxkwQZJfgnq9nsZf6b1BXvTAeKP5aiAmcfUBZQFensN7DDY6Ea2QAy1NViZ9i4hbr1smhchHuPo3TkYDKm --password ikuk1 --algorithm turtlecoin
